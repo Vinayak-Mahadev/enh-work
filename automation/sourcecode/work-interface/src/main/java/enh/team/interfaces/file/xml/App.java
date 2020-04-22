55710386de476cd0f506868540c4dda1a117a3c9
Sreerag <sreerag.a@enhancesys.com>
2020-04-22 14:40:36 +0530

    Property Changes

--- automation/sourcecode/work-interface/src/main/java/enh/team/interfaces/file/xml/App.java
+++ automation/sourcecode/work-interface/src/main/java/enh/team/interfaces/file/xml/App.java
@@ -2,7 +2,8 @@ package enh.team.interfaces.file.xml;
 
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		XmlOperation.testJsonXmlDataConverterLocal();
 	}
 }
