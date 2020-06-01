32b57cb4bca2a8466fc3537ac85849a077d91325
Sreerag <sreerag.a@enhancesys.com>
2020-06-01 12:04:28 +0530

    Configured spring mvc

--- JobEngine/service/jobengine-consumer/src/main/java/com/enhancesys/jobengine/consumer/controller/JobConsumerController.java
+++ JobEngine/service/jobengine-consumer/src/main/java/com/enhancesys/jobengine/consumer/controller/JobConsumerController.java
@@ -3,25 +3,26 @@ package com.enhancesys.jobengine.consumer.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value = "/consumer")
 public class JobConsumerController 
 {
 
-	@GetMapping("/")
-	public String home() 
+	@GetMapping("/home")
+	public ModelAndView home() 
 	{
-		String responce = "Welcome to Job Consumer Service";
+		String response = "Welcome to Job Consumer Service";
 		try 
 		{
-
+			
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
-		return responce;
+		return new ModelAndView("welcome", "data", response);
 	}
 
 }
