41492fdca2502fb17655540414da33f78057111c
Vinayak-Mahadev <vinay.nagaraj@enhancesys.com>
2020-06-08 09:25:53 +0530

    Update

--- JobEngine/service/job-consumer/src/main/java/com/enhancesys/jobengine/consumer/services/JobEngineServices.java
+++ JobEngine/service/job-consumer/src/main/java/com/enhancesys/jobengine/consumer/services/JobEngineServices.java
@@ -15,7 +15,7 @@ public class JobEngineServices
 		{
 			//JobServcies.proceeRequest((JSONObject) new JSONParser().parse("{\"job-id\":\"FossApprovalReport\",\"template-id\":\"Sample-feed\"}"));
 			//JobServcies.proceeRequest((JSONObject) new JSONParser().parse("{\"job-id\":\"postgres_Sample\",\"template-id\":\"Productive-outlet\"}"));
-			//JobServcies.proceeRequest((JSONObject) new JSONParser().parse("{\"job-id\":\"postgres_Sample\",\"template-id\":\"OrgDetails-feed\"}"));
+			JobServcies.proceeRequest((JSONObject) new JSONParser().parse("{\"job-id\":\"Sample-OrgDetailsReport\",\"template-id\":\"OrgDetails-feed\"}"));
 			//JobServcies.proceeRequest((JSONObject) new JSONParser().parse("{\"job-id\":\"Sample-csv-output\",\"template-id\":\"Jasper-feed\"}"));
 			//JobServcies.proceeRequest((JSONObject) new JSONParser().parse("{\"job-id\":\"postgres_Sample\",\"template-id\":\"DataList-Outlet\"}"));
 			//JobServcies.proceeRequest((JSONObject) new JSONParser().parse("{\"job-id\":\"postgres_Sample\",\"template-id\":\"Sample-report-feed\"}"));
