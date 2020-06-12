951fd6c77504bfd6a7e4bcb81e75ce641881e5e7
Sreerag <sreerag.a@enhancesys.com>
2020-06-12 13:14:40 +0530

    1179 and 1180 prequery changes

--- daily_works/2020-06/hadoop_feeds/hadoop-interface-update_v5/hadoop_feed_2nd_v/hadoop-interface_v3/pre-aggregate-queries_v3/1179_monthly_pre_aggr_query.sql
+++ daily_works/2020-06/hadoop_feeds/hadoop-interface-update_v5/hadoop_feed_2nd_v/hadoop-interface_v3/pre-aggregate-queries_v3/1179_monthly_pre_aggr_query.sql
@@ -1,2 +1,2 @@
 --1179_MONTHLY_PRE_AGGR_QUERY= 
-delete from kpi.tr_monthly_outlet_program_aggr where month_id_n in (select distinct(id_n) from kpi.tr_temp_outlet_program_aggr where file_id_n = ?);
+delete from kpi.tr_monthly_outlet_program_aggr where month_id_n in (select distinct(to_char(to_date(id_n::text,'yyyymmdd'),'yyyymm')::numeric) from kpi.tr_temp_outlet_program_aggr where file_id_n = ?); 
--- daily_works/2020-06/hadoop_feeds/hadoop-interface-update_v5/hadoop_feed_2nd_v/hadoop-interface_v3/pre-aggregate-queries_v3/1180_monthly_pre_aggr_query.sql
+++ daily_works/2020-06/hadoop_feeds/hadoop-interface-update_v5/hadoop_feed_2nd_v/hadoop-interface_v3/pre-aggregate-queries_v3/1180_monthly_pre_aggr_query.sql
@@ -1,2 +1,2 @@
 --1180_MONTHLY_PRE_AGGR_QUERY= 
-delete from kpi.tr_monthly_alloc_payment_aggr where month_id_n in (select distinct(id_n) from kpi.tr_temp_alloc_payment_aggr where file_id_n = ?);
+delete from kpi.tr_monthly_alloc_payment_aggr where month_id_n in (select distinct(to_char(to_date(id_n::text,'yyyymmdd'),'yyyymm')::numeric) from kpi.tr_temp_alloc_payment_aggr where file_id_n = ?); 
